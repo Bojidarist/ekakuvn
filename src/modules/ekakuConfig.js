2ddcd57660aa76f5e7037dd8a8e4e715c555bda2
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2025-06-23 23:04:10 +0300

    Format config code

--- src/modules/ekakuConfig.js
+++ src/modules/ekakuConfig.js
@@ -1,42 +1,42 @@
 export class EkakuConfig {
-    #config = {};
+    #config = {}
     constructor(storageKey = "ekakuConfig") {
-        this.storageKey = storageKey;
-        this.#load();
+        this.storageKey = storageKey
+        this.#load()
     }
 
     #load() {
         try {
-            this.#config = JSON.parse(localStorage.getItem(this.storageKey)) || {};
+            this.#config = JSON.parse(localStorage.getItem(this.storageKey)) || {}
         } catch {
-            this.#config = {};
+            this.#config = {}
         }
     }
 
     #save() {
-        localStorage.setItem(this.storageKey, JSON.stringify(this.#config));
+        localStorage.setItem(this.storageKey, JSON.stringify(this.#config))
     }
 
     set(k, v) {
-        this.#config[k] = v;
-        this.#save();
+        this.#config[k] = v
+        this.#save()
     }
 
     get(k) {
-        return this.#config[k];
+        return this.#config[k]
     }
 
     getAll() {
-        return { ...this.#config };
+        return { ...this.#config }
     }
 
     remove(k) {
-        delete this.#config[k];
-        this.#save();
+        delete this.#config[k]
+        this.#save()
     }
 
     clear() {
-        this.#config = {};
-        localStorage.removeItem(this.storageKey);
+        this.#config = {}
+        localStorage.removeItem(this.storageKey)
     }
 }
